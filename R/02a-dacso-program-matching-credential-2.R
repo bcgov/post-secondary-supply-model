5914371f3f180cf38878ba845bf321e7757c2e6e
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-04 09:42:01 -0700

    refactor

--- R/02a-dacso-program-matching-credential-2.R
+++ R/02a-dacso-program-matching-credential-2.R
@@ -1,16 +1,7 @@
-# dbo_STP_Credential_Non_Dup_Programs_DACSO (created in previous step and imported from SSMS)
-# INFOWARE_PROGRAMS_HIST_PRGMID_XREF
-# INFOWARE_C_OUTC_CLEAN_SHORT_RESP - I don't think we need this one
-# INFOWARE_L_CIP_2DIGITS_CIP2016
-# INFOWARE_L_CIP_4DIGITS_CIP2016
-# INFOWARE_L_CIP_6DIGITS_CIP2016
-# INFOWARE_PROGRAMS
+# ---- Required Tables (Access) ---- 
+# dbo_STP_Credential_Non_Dup_Programs_DACSO
+# tbl_STP_PSI_CODE_INST_CD_Lookup 
 # DACSO_STP_ProgramsCIP4_XWALK_ALL_2018
-
-# DACSO_Programs_Seen_02_to_18                           
-# STP_Credential_Non_Dup_Programs_DACSO_CIPS_CHANGED_2018 
-# tbl_STP_PSI_CODE_INST_CD_Lookup    
-# tbl_SubmCd_Lookup  
 
 library(tidyverse)
 library(RODBC)
@@ -22,4 +13,30 @@ source("./sql/02a-dacso-program-matching.R")
 connection <- config::get("connection")$outcomes_dacso
 con <- odbcDriverConnect(connection)
 
-sqlTables(con,  tableType = c("TABLE")) %>% pull(TABLE_NAME) # tableType = "SYNONYM" gives tables linked to ORACLE
+# readr::read_delim(file = "./cred_non_dup.txt", delim = "\t", col_names = TRUE)
+
+# ---- Restructure and populate imported STP data ---- 
+sqlQuery(con, "ALTER TABLE STP_Credential_Non_Dup_Programs_DACSO 
+               ADD COLUMN Already_Matched VARCHAR(255), New_Auto_Match VARCHAR(255), New_Manual_Match VARCHAR(255);")
+
+sqlQuery(con, "UPDATE dbo_STP_Credential_Non_Dup_Programs_DACSO 
+INNER JOIN tbl_STP_PSI_CODE_INST_CD_Lookup ON dbo_STP_Credential_Non_Dup_Programs_DACSO.PSI_CODE = tbl_STP_PSI_CODE_INST_CD_Lookup.PSI_CODE 
+SET dbo_STP_Credential_Non_Dup_Programs_DACSO.COCI_INST_CD = [tbl_STP_PSI_CODE_INST_CD_Lookup].[COCI_INST_CD];")
+
+# ---- Matched programs with new CIPs ----
+sqlQuery(con, qry_STP_Credential_DACSO_Programs_AlreadyMatched)
+sqlQuery(con, qry_STP_Credential_DACSO_Programs_AlreadyMatched_b)
+
+# ---- Newly matched programs with new CIPS ----
+sqlQuery(con, qry_STP_Credential_DACSO_Programs_NewMatches_a)
+sqlQuery(con, qry_STP_Credential_DACSO_Programs_NewMatches_a_step2)
+sqlQuery(con, qry_STP_Credential_DACSO_Programs_NewMatches_b)
+sqlQuery(con, qry_STP_Credential_DACSO_Programs_NewMatches_b_step2)
+
+# ---- Search for programs requiring manual work ----
+# This query is missing and needs to be created - see documentation
+sqlQuery(con, qry_STP_Cred_DACSO_Programs_to_Match)
+sqlQuery(con, qry_Make_DACSO_Programs_Seen)
+
+
+
