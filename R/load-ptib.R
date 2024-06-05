f7fdf87c215f9c02ded514754e92195e79a7f7c2
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2024-06-05 15:57:57 -0700

    add formatting/aggregation to match 2017/18 data

--- R/load-ptib.R
+++ R/load-ptib.R
@@ -15,13 +15,24 @@ lan <- get_network_path()
 ptib <- glue::glue("{lan}/data/ptib/")
 
 ## ----- Raw data file  ----
-raw_data <- glue::glue("{ptib}/PTIB 2021 and 2022 Enrolment Data for BC Stats 2024.05.31.xlsx")
+raw_data_file <- glue::glue("{ptib}/PTIB 2021 and 2022 Enrolment Data for BC Stats 2024.05.31.xlsx")
 
 ## ----- Read raw data  ----
-data <- xlsx::read.xlsx(raw_data, sheetIndex = 1, startRow = 3) %>% 
+raw_data <- xlsx::read.xlsx(raw_data_file, sheetIndex = 1, startRow = 3) %>% 
   janitor::clean_names() %>% 
   rename(graduates = credential_1) ## fairly sure this is the case since this 
                                    ## column is equal to total_enrolments - enrolments_not_graduated
+
+## ----- Aggregate data ----
+data <- raw_data %>%
+  group_by(calendar_year, credential, cip, age_group, immigration_status) %>%
+  summarize(`Sum of Graduates` = sum(graduates, na.rm = TRUE),
+            `Sum of Enrolments` = sum(enrolments_not_graduated, na.rm = TRUE),
+            `Sum of Total Enrolments` = sum(total_enrolments, na.rm = TRUE),
+            .groups = "drop") %>%
+  select(Year = calendar_year,	`Credential (Program) (Program)` = credential,
+         CIP = cip,	`Age Group` = age_group, `Immigration Status` = immigration_status,
+         `Sum of Graduates`,	`Sum of Enrolments`, 	`Sum of Total Enrolments`)
 
 ## ----- Connection to decimal ----
 db_config <- config::get("decimal")
@@ -32,17 +43,15 @@ con <- dbConnect(odbc(),
                  Trusted_Connection = "True")
 
 # ---- Define Schema ----
-schema <- 
-  schema(calendar_year = float(),
-         institution = string(),
-         program = string(),
-         cip = string(),
-         age_group = string(),
-         credential = string(),
-         immigration_status = string(),
-         graduates = float(),            
-         enrolments_not_graduated = float(),
-         total_enrolments = float())
+schema <-
+  schema(Year = float(),
+         `Credential (Program) (Program)` = string(),
+         CIP = string(),
+         `Age Group` = string(),
+         `Immigration Status` = string(),
+         `Sum of Graduates` = float(),
+         `Sum of Erolments` = float(),
+         `Sum of Total Enrolments` = float())
 
 # ---- Write to decimal ----
 dbWriteTableArrow(con,
