aa2354894e62284ec10b369f4dc5005683284604
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-06 14:45:27 -0700

    add file for bgs cohort

---
+++ R/pssm-cohort-bgs.R
@@ -0,0 +1,33 @@
+# ---- Required Tables ----
+# INFOWARE_BGS_COHORT_INFO
+# INFOWARE_BGS_DIST_xx_yy
+# T_BGS_Data_Final (from last runs PSSM)
+# several other smaller tables which appear to be derived in earlier steps
+
+# INFOWARE_BGS_PROGRAMS
+#
+library(tidyverse)
+library(RODBC)
+library(config)
+library(glue)
+library(DBI)
+
+#lan <- safepaths::get_network_path()
+source(glue("{lan}/development/sql/gh-source/02b-pssm-cohort-bgs.R"))
+
+#---- Connect to Outcomes Database ----
+connection <- config::get("connection")$outcomes_cohorts
+con <- odbcDriverConnect(connection)
+
+sqlQuery(con, BGS_Q001_BGS_Data_2018_2019)
+sqlQuery(con, BGS_Q001b_INST_Recode)
+sqlQuery(con, BGS_Q001c_Update_CIPs_After_Program_Matching)
+sqlQuery(con, BGS_Q002_LCP4_CRED)
+sqlQuery(con, BGS_Q003b_Add_CURRENT_REGION_PSSM)
+sqlQuery(con, BGS_Q003b_Add_CURRENT_REGION_PSSM2)
+sqlQuery(con, BGS_Q003c_Derived_And_Weights)
+sqlQuery(con, BGS_Q005_1b1_Delete_Cohort)
+sqlQuery(con, BGS_Q005_1b2_Cohort_Recoded)
+sqlQuery(con, BGS_Q99A_ENDDT_IMPUTED)
+                
+close(con)
