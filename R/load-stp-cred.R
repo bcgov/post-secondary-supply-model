0778ac9ea76e569dfa86cf8f49e4d1b2fddce638
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-14 14:32:35 -0700

    refactor and clean up

--- R/load-stp-cred.R
+++ R/load-stp-cred.R
@@ -9,17 +9,11 @@ library(arrow)
 library(tidyverse)
 library(odbc)
 library(DBI)
-library(safepaths)
 library(config)
 
-# ---- Configure LAN Paths and DB Connection ----
-lan <- get_network_path()
-stp_2023 <- glue::glue("{lan}/data/stp/")
-
-## ----- Raw data file (remove if not needed) ---- 
-i <- grepl("2019-2020", list.files(dirname(lan)))
-stp_2019 <- glue::glue("{list.files(dirname(lan), full.names = TRUE)[i]}/Data/STP")
-raw_data <- glue::glue("{stp_2019}/STP_Credential.txt")
+# ---- Configure LAN and file paths ----
+lan <- config::get("lan")
+raw_data <- glue::glue("{lan}/data/stp/STP_ISA_PSSM/STP_CREDENTIAL_2023.dsv")
 
 ## ----- Connection to decimal ----
 db_config <- config::get("decimal")
@@ -29,35 +23,41 @@ con <- dbConnect(odbc(),
                  Database = db_config$database,
                  Trusted_Connection = "True")
 
-
 # ---- Define Schema ----
 schema <- 
-  schema(PTY_ID = string(),
-         CREDENTIAL_AWARD_DATE= string(),
-         PSI_CODE= string(),
-         PSI_FULL_NAME= string(),
-         PSI_PEN= string(),
-         PSI_STUDENT_NUMBER= string(),
-         PSI_SCHOOL_YEAR= string(),
-         PSI_PROGRAM_CODE= string(),
-         PSI_PROGRAM_EFFECTIVE_DATE= string(),
-         PSI_CREDENTIAL_CATEGORY=string(),
-         PSI_CREDENTIAL_LEVE= string(),
-         PSI_CREDENTIAL_CIP= string(),
-         PSI_CREDENTIAL_PROGRAM_DESC= string(),
-         ENCRYPTED_TRUE_PEN= string(),
-         STP_ALT_ID= string())
+  schema(CREDENTIAL_AWARD_DATE = string(),
+         PSI_CODE = string(),
+         PSI_FULL_NAME = string(),
+         PSI_PEN = string(),
+         PSI_STUDENT_NUMBER = string(),
+         PSI_SCHOOL_YEAR = string(),
+         PSI_PROGRAM_CODE = string(),
+         PSI_PROGRAM_EFFECTIVE_DATE = string(),
+         PSI_CREDENTIAL_CATEGORY =string(),
+         PSI_CREDENTIAL_LEVEL = string(),
+         PSI_CREDENTIAL_CIP = string(),
+         PSI_CREDENTIAL_PROGRAM_DESCRIPTION = string(),
+         SNAPSHOT_DATE = string(),
+         ENCRYPTED_TRUE_PEN = string(),
+         STP_ALT_ID = string())
 
 # ---- Write to decimal ----
+tblnm <- tools::file_path_sans_ext(basename(raw_data))
+cat(glue::glue("Processing {tblnm}: {Sys.time()} ..."))
+
 data <- open_dataset(
   sources = raw_data, 
-  col_types = schema,
-  format = "tsv"
+  format = "tsv",
+  schema = schema,
+  skip = 1
 )
 
-print(glue::glue("Processing STP_Credential"))
-dbWriteTableArrow(con, name = "STP_Credential", 
-                  nanoarrow::as_nanoarrow_array_stream(data)) # also, dbWriteTableArrow takes a schema()
+dbWriteTableArrow(con, 
+                  name = "STP_Credential", 
+                  nanoarrow::as_nanoarrow_array_stream(data), append = TRUE)
+
+cat(glue::glue("...completed {Sys.time()}"))
+cat("\n")
 
 
 
