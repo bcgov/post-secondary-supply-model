10828eac6338d3d1f8bc1dd7dea5b03e19653ad9
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-14 07:09:14 -0700

    minor edits and comments

--- R/pssm-cohort-trd.R
+++ R/pssm-cohort-trd.R
@@ -3,22 +3,25 @@
 # tbl_Age 
 # tbl_Age_Groups
 
-
 library(tidyverse)
 library(RODBC)
 library(config)
 library(glue)
 library(DBI)
 
-lan <- config::get("lan_citrix")
-source(glue("{lan}/development/sql/gh-source/02b-pssm-cohorts-trd.R"))
+lan <- config::get("lan")
+lan_alder <- config::get("lan_alder")
+
+source(glue::glue("{lan}/development/sql/gh-source/02b-pssm-cohorts-trd.R"))
 
 #---- Connect to Outcomes Database ----
 connection <- config::get("connection")$outcomes_cohorts
 con <- odbcDriverConnect(connection)
 
 # trd data from primary tables
-sqlQuery(con, `000_TRD_DATA_01`)  
+sqlQuery(con, `Q000_TRD_DATA_01`)  
+# age and age_group variables are joined from lookups not in SO db
+# LCIP4_CRED variable is derived from other vars
 
 # aggregated counts from primary tables
 sqlQuery(con, `000_TRD_Graduates`)
