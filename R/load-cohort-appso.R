dfbb3ce151d5a841536274850d01e163d305e989
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-18 09:32:00 -0700

    add connections and logic

--- R/load-cohort-appso.R
+++ R/load-cohort-appso.R
@@ -2,20 +2,44 @@
 # Primary Outcomes tables: See raw data documentation
 # tbl_age
 # tbl_age_groups
-
-
 library(tidyverse)
 library(RODBC)
 library(config)
 library(glue)
 library(DBI)
+library(RJDBC)
 
+# ---- Configure LAN and file paths ----
+db_config <- config::get("pdbtrn")
+jdbc_driver_config <- config::get("jdbc")
 lan <- config::get("lan")
-source(glue("{lan}/development/sql/gh-source/02b-pssm-cohorts-appso.R"))
 
-#---- Connect to Outcomes Database ----
-connection <- config::get("connection")$outcomes_cohorts
-con <- odbcDriverConnect(connection)
+# ---- Connection to outcomes ----
+jdbcDriver <- JDBC(driverClass = jdbc_driver_config$class,
+                   classPath = jdbc_driver_config$path)
 
+outcomes_con <- dbConnect(drv = jdbcDriver, 
+                 url = db_config$url,
+                 user = db_config$user,
+                 password = db_config$password)
 
-close(con)
+# ---- Read raw data and disconnect ----
+source(glue("{lan}/data/student-outcomes/sql/APPSO_DATA_01_Final.sql"))
+source(glue("{lan}/data/student-outcomes/sql/APPSO_Graduates.sql"))
+
+APPSO_DATA_01_Final <- dbGetQuery(outcomes_con, APPSO_DATA_01_Final)
+APPSO_Graduates <- dbGetQuery(outcomes_con, APPSO_Graduates)
+
+dbDisconnect(outcomes_con)
+
+# ---- Connection to decimal ----
+db_config <- config::get("decimal")
+decimal_con <- dbConnect(odbc::odbc(),
+                 Driver = db_config$driver,
+                 Server = db_config$server,
+                 Database = db_config$database,
+                 Trusted_Connection = "True")
+dbWriteTable(decimal_con, name = "APPSO_DATA_01_Final", value = APPSO_DATA_01_Final)
+dbWriteTable(decimal_con, name = "APPSO_Graduates", value = APPSO_Graduates)
+
+dbDisconnect(decimal_con)
