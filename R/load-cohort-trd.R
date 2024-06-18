eef8348e3f6afcd966b4045eba97ae00222121d8
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-18 09:38:01 -0700

    add connections and logic

--- R/load-cohort-trd.R
+++ R/load-cohort-trd.R
@@ -3,27 +3,48 @@
 # tbl_Age 
 # tbl_Age_Groups
 
+# ******************************************************************************
+
+# ******************************************************************************
+
 library(tidyverse)
 library(RODBC)
 library(config)
 library(glue)
 library(DBI)
+library(RJDBC)
 
+# ---- Configure LAN and file paths ----
+db_config <- config::get("pdbtrn")
+jdbc_driver_config <- config::get("jdbc")
 lan <- config::get("lan")
-lan_alder <- config::get("lan_alder")
 
-source(glue::glue("{lan}/development/sql/gh-source/02b-pssm-cohorts-trd.R"))
+# ---- Connection to outcomes ----
+jdbcDriver <- JDBC(driverClass = jdbc_driver_config$class,
+                   classPath = jdbc_driver_config$path)
 
-#---- Connect to Outcomes Database ----
-connection <- config::get("connection")$outcomes_cohorts
-con <- odbcDriverConnect(connection)
+outcomes_con <- dbConnect(drv = jdbcDriver, 
+                          url = db_config$url,
+                          user = db_config$user,
+                          password = db_config$password)
 
-# trd data from primary tables
-sqlQuery(con, `Q000_TRD_DATA_01`)  
-# age and age_group variables are joined from lookups not in SO db
-# LCIP4_CRED variable is derived from other vars
+# ---- Read raw data and disconnect ----
+source(glue("{lan}/data/student-outcomes/sql/Q000_TRD_DATA_01.sql"))
+source(glue("{lan}/data/student-outcomes/sql/Q000_TRD_Graduates.sql"))
 
-# aggregated counts from primary tables
-sqlQuery(con, `000_TRD_Graduates`)
-                
-close(con)
+Q000_TRD_DATA_01 <- dbGetQuery(outcomes_con, Q000_TRD_DATA_01)
+Q000_TRD_Graduates <- dbGetQuery(outcomes_con, Q000_TRD_Graduates)
+
+dbDisconnect(outcomes_con)
+
+# ---- Connection to decimal ----
+db_config <- config::get("decimal")
+decimal_con <- dbConnect(odbc::odbc(),
+                         Driver = db_config$driver,
+                         Server = db_config$server,
+                         Database = db_config$database,
+                         Trusted_Connection = "True")
+dbWriteTable(decimal_con, name = "Q000_TRD_DATA_01", value = Q000_TRD_DATA_01)
+dbWriteTable(decimal_con, name = "Q000_TRD_Graduates", value = Q000_TRD_Graduates)
+
+dbDisconnect(decimal_con)
