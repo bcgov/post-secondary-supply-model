e3b9a4e6c1dc3ba21f97da70ee0dcf5128be50d0
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-06-25 08:40:45 -0700

    drop tables and add last queries

--- R/01a-credential-preprocessing.R
+++ R/01a-credential-preprocessing.R
@@ -11,6 +11,7 @@ source(glue::glue("{lan}/development/sql/gh-source/01a-credential-preprocessing/
 # set connection string to decimal
 db_config <- config::get("decimal")
 db_schema <- config::get("dbschema")
+my_schema <- config::get("myschema")
 
 con <- dbConnect(odbc(),
                  Driver = db_config$driver,
@@ -20,6 +21,8 @@ con <- dbConnect(odbc(),
 
 # ---- A few checks ----
 dbExistsTable(con, SQL(glue::glue('"{db_schema}"."STP_CREDENTIAL"')))
+dbExistsTable(con, SQL(glue::glue('"{db_schema}"."STP_Enrolment_Record_Type"')))
+dbExistsTable(con, SQL(glue::glue('"{db_schema}"."STP_Enrolment"')))
 dbGetQuery(con, qry00a_check_null_epens)
 dbGetQuery(con, qry00b_check_unique_epens)
 
@@ -56,46 +59,59 @@ dbExecute(con, "DROP TABLE tmp_ConvertDateFormatCredential")
 # 7 = Developmental CIP
 # 8 = Recommendation for Certification 
 
-# Create lookup table for ID/Record Status and populate with ID column and EPEN 
+# ---- Create lookup table for ID/Record Status and populate with ID column and EPEN ----
 dbExecute(con, qry01_ExtractAllID_into_STP_Credential_Record_Type)
 
-# Find records with Record_Status = 0 and update look up table
+# ---- Find records with Record_Status = 1 and update look up table ----
 dbExecute(con, qry02a_Record_With_PEN_Or_STUID)
-
-# Find records with Record_Status = 1 and update look up table
 dbExecute(con, qry02b_Drop_No_PEN_Or_No_STUID)
 dbExecute(con, qry02c_Update_Drop_No_PEN_or_No_STUID)
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[tmp_tbl_qry02a_Cred_Record_With_PEN_or_STUID];"))
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Drop_Cred_No_PEN_or_No_STUID];")) 
 
 # Find records with Record_Status = 2 and update look up table
 dbExecute(con, qry03a_Drop_Record_Developmental)
 dbExecute(con, qry03b_Update_Drop_Record_Developmental)
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Drop_Cred_Developmental];")) 
 
 # Create a subset of potential Record_Status = 6 records that have not already assigned a record status
-dbExistsTable(con, SQL(glue::glue('"{db_schema}"."STP_Enrolment_Record_Type"')))
-dbExistsTable(con, SQL(glue::glue('"{db_schema}"."STP_Enrolment"')))
-
 dbExecute(con, qry03c_create_table_EnrolmentSkillsBasedCourse)
-# dbExecute(con, qry03c_Drop_Skills_Based) # is this run?
+dbExecute(con, qry03c_Drop_Skills_Based)
+dbExecute(con, qry03d_Update_Drop_Record_Skills_Based)
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Drop_Skills_Based];"))
+
 dbExecute(con, qry03d_create_table_Suspect_Skills_Based)
-# dbExecute(con, qry03d_Update_Drop_Record_Skills_Based) # is this run?
 dbExecute(con, qry03e_Find_Suspect_Skills_Based)
-# dbExecute(con, qry03e_Keep_Skills_Based) # is this run?
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[tmp_tbl_Cred_Suspect_Skills_Based];")) 
+dbExecute(con, qry03e_Keep_Skills_Based)
 dbExecute(con, qry03f_Update_Keep_Record_Skills_Based)
 dbExecute(con, qry03f_Update_Suspect_Skills_Based)
-# dbExecute(con, qry03g_create_table_SkillsBasedCourses) # is this run?
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Keep_Skills_Based];"))
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Cred_Suspect_Skills_Based];")) 
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[tmp_tbl_EnrolmentSkillsBasedCourses];")) 
  
-# Find records with Record_Status = 7 and update look up table
+# ---- Find records with Record_Status = 7 and update look up table ----
 dbExecute(con, qry03g_Drop_Developmental_Credential_CIPS)
+dbExecute(con, "ALTER TABLE Drop_Developmental_PSI_CREDENTIAL_CIPS ADD KEEP NVARCHAR(2)")
+## ---- Manual Work ----
+data <- dbReadTable(con, "Drop_Developmental_PSI_CREDENTIAL_CIPS", col_types = cols(.default = col_character()))
+data <- data %>% mutate(KEEP = NA) # or use excel, utils::data.entry or whatever
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Drop_Developmental_PSI_CREDENTIAL_CIPS];")) 
+dbWriteTable(con, name = "Drop_Developmental_PSI_CREDENTIAL_CIPS", data)
 
-# ---- TODO: add a column Keep (see documentation)
+## ---- Back to Workflow ----
 dbExecute(con, qry03h_Update_Developmental_CIPs)
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Drop_Developmental_PSI_CREDENTIAL_CIPS];")) 
 
-# Find records with Record_Status = 8 and update look up table
+# ---- Find records with Record_Status = 8 and update look up table ----
 dbExecute(con, qry03i_Drop_RecommendationForCert)
 dbExecute(con, qry03j_Update_RecommendationForCert)
+dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[Drop_Cred_RecommendForCert];")) 
 
+# ---- Investigate This Query ----
 # dbExecute(con, qry03k_Drop_Developmental_CIPS) # are these run?
 # dbExecute(con, qry03k_Update_ID_for_Drop_Dev_Credential_CIP) # are these run?
+# dbExecute(con, glue::glue("DROP TABLE [{my_schema}].[];")) 
 
 dbExecute(con, qry04_Update_RecordStatus_Not_Dropped)
 
