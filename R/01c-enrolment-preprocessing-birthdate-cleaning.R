8761ebb6ce80754205fef99d45cbd0b57cd79090
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-07-03 11:07:45 -0700

    update statements

--- R/01c-enrolment-preprocessing-birthdate-cleaning.R
+++ R/01c-enrolment-preprocessing-birthdate-cleaning.R
@@ -50,24 +50,33 @@ dbExecute(con, "ALTER TABLE tmp_MoreThanOne_Birthdate
           NumMaxBirthdateRecords INT")
 
 # update the tmp_MoreThanOne_Birthdate table columns for the min bdtates
-dbExecute(con, qry07_BirthdateCleaning)
-dbExecute(con, "ALTER TABLE tmp_MoreThanOne_Birthdate ADD LAST_SEEN_BIRTHDATE VARCHAR(19)")
+dbExecute(con, qry07a_BirthdateCleaning)
 
+# update the tmp_MoreThanOne_Birthdate table columns for the max bdtates  
+dbExecute(con, qry07b_BirthdateCleaning)
+
+dbExecute(con, "ALTER TABLE tmp_MoreThanOne_Birthdate ADD LastSeenBirthdate VARCHAR(19)")
 dbExecute(con, qry08_BirthdateCleaning)
 
+dbExecute(con, "ALTER TABLE tmp_MoreThanOne_Birthdate 
+          ADD MaxOrMin_MostCommon VARCHAR(50),
+          Year_Max INT,
+          Year_Min INT,
+          YearDif_MaxMinusMin INT 
+          UseMaxOrMin_FINAL VARCHAR(50), 
+          MaxOrMin_MostCommon VARCHAR(50)")
+
+dbExecute(con, "UPDATE tmp_MoreThanOne_Birthdate 
+          SET Year_Min = YEAR(MinPSIBirthdate),
+          Year_Max = YEAR(MaxPSIBirthdate), 
+          YearDif_MaxMinusMin = YEAR(MaxPSIBirthdate) - YEAR(MinPSIBirthdate)")
+
 # ---- Manual Work ----
+# decided whether to use max or min birthdate based on the frequency of birthdate year. I think we can pull this into R and take the mode
 tmp_Clean_MaxMinBirthDate <- dbReadTable(con, "tmp_MoreThanOne_Birthdate")
+readr::write_csv(glue::glue("{lan}/development/csv/gh-source/01c-clean-max-min-birthdate.csv"))
 tmp_Clean_MaxMinBirthDate <- readr::read_csv(glue::glue("{lan}/development/csv/gh-source/01c-clean-max-min-birthdate.csv"), col_types = cols(.default = col_character()))
 dbWriteTable(con, name = "tmp_Clean_MaxMinBirthDate", tmp_Clean_MaxMinBirthDate)
-
-dbExecute(con, "ALTER TABLE tmp_MoreThanOne_Birthdate 
-          ADD --MaxOrMin_MostCommon VARCHAR(50),
-          --Year_Max VARCHAR(50),
-          --Year_Min VARCHAR(50),
-          --YearDif_MaxMinusMin VARCHAR(50), 
-          --UseMaxOrMin_FINAL VARCHAR(50), 
-          --YearDif_MaxMinusMin VARCHAR(50), 
-          --MaxOrMin_MostCommon VARCHAR(50)")
 
 # Update these new columns in the tmp_MoreThanOne_Birthdate 
 dbExecute(con, qry09_BirthdateCleaning)
@@ -91,6 +100,7 @@ dbExecute(con, qry14_BirthdateCleaning)
 dbExecute(con, qry15_BirthdateCleaning)
 
 #add a column called psi_birthdate_cleaned (varchar(19) to tmp_NullBirthdateCleaned 
+dbExecute(con, "ALTER TABLE tmp_NullBirthdateCleaned ADD psi_birthdate_cleaned varchar(19)")
 
 # Populate the cleaned birthdate with the min birthdate for EPENs with multi-birthdates
 dbExecute(con, qry16_BirthdateCleaning)
@@ -109,4 +119,15 @@ dbExecute(con, qry20_BirthdateCleaning)
 
 # Finds any PSI_STUDENT_NUMBER/PSI_CODE combos associated with >1 birthdate
 dbExecute(con, qry21_BirthdateCleaning)
+
+dbExecute(con , "DROP TABLE tmp_BirthDate")
+dbExecute(con , "DROP TABLE tmp_Clean_MaxMinBirthDate")
+dbExecute(con , "DROP TABLE tmp_MaxPSIBirthdate")
+dbExecute(con , "DROP TABLE tmp_MinPSIBirthdate")
+dbExecute(con , "DROP TABLE tmp_MoreThanOne_Birthdate")
+
+dbExecute(con , "DROP TABLE tmp_NullBirthDate")
+dbExecute(con , "DROP TABLE tmp_NonNullBirthDate")
+dbExecute(con , "DROP TABLE tmp_NullBirthDateCleaned")
+dbExecute(con , "DROP TABLE tmp_TEST_multi_birthdate")
 
