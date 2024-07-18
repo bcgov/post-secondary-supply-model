91e5bcd130fa5e05edfcaec4f530bc97931f457e
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-07-17 19:25:07 -0700

    add dbExecute statments

--- R/02b-pssm-cohorts-bgs.R
+++ R/02b-pssm-cohorts-bgs.R
@@ -18,19 +18,36 @@ decimal_con <- dbConnect(odbc::odbc(),
                          Database = db_config$database,
                          Trusted_Connection = "True")
 
-# ---- Connection to access ----
-
-
-
 # ---- Check for required data tables ----
 dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_BGS_Data_Final"')))
 dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_BGS_INST_Recode"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_bgs_data_final_for_outcomesmatching2020"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_Weights"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."bgs_current_region_data"'))) 
 
-#Notes: watch for Age_Grouping variable, Ian mentiones having removed it from earlier queries, to be linked later. 
+# Notes: watch for Age_Grouping variable, documentation mentions having removed it from earlier queries and linked later.  not sure what this means.
 
+# Recode institution codes to be consistent to STP file
+dbExecute(decimal_con, BGS_Q001b_INST_Recode)
 
 # Note: update CIPS after program matching.  Some eyes needed to double check method.
+dbExecute(decimal_con, BGS_Q001c_Update_CIPs_After_Program_Matching)
+dbExecute(decimal_con, BGS_Q002_LCP4_CRED)
 
+# updates CURRENT_REGION_PSSM_CODE after the geocoding.
+dbExecute(decimal_con, BGS_Q003b_Add_CURRENT_REGION_PSSM)
+dbExecute(decimal_con, BGS_Q003b_Add_CURRENT_REGION_PSSM2)
+
+# Applies weight for model year and derives New Labour Supply
+# Note: SQl may need to be updated - looks like the query needs a case_when added
+dbExecute(decimal_con, BGS_Q003c_Derived_And_Weights)
+
+# Refresh bgs survey records in T_Cohorts_Recoded
+dbExecute(decimal_con, BGS_Q005_1b1_Delete_Cohort)
+dbExecute(decimal_con, BGS_Q005_1b2_Cohort_Recoded)
+
+# Note: the following query may not be used
+dbExecute(decimal_con, BGS_Q99A_ENDDT_IMPUTED)
 
 # ---- Clean Up ----
 dbDisconnect(decimal_con)
--- R/02b-pssm-cohorts-dacso.R
+++ R/02b-pssm-cohorts-dacso.R
@@ -25,5 +25,37 @@ dbExistsTable(con, SQL(glue::glue('"{my_schema}"."t_pssm_credential_grouping"'))
 dbExistsTable(con, SQL(glue::glue('"{my_schema}"."infoware_c_outc_clean_short_resp"')))
 dbExistsTable(con, SQL(glue::glue('"{my_schema}"."infoware_c_outc_clean2"')))
 
+# Notes: watch for Age_Grouping variable, documentation mentions having removed it from earlier queries and linked later.  not sure what this means.
+
+# Recodes CIP codes
+dbExecute(decimal_con, DACSO_Q003_DACSO_Data_Part_1_stepB)
+
+# Recode institution codes for CIP-NOC work
+dbExecute(decimal_con, DACSO_Q003b_DACSO_DATA_Part_1_Further_Ed)
+
+# Deletes other, none, invalid etc. credentials that are not part of the PSSM
+dbExcute(decimal_con, DACSO_Q004_DACSO_DATA_Part_1_Delete_Credentials)
+
+# Recodes all the old institution codes to the current code so that weight adjustments across years by program can be applied
+dbExcute(decimal_con, DACSO_Q004b_INST_Recode)
+
+# updates CURRENT_REGION_PSSM_CODE after the geocoding.
+dbExcute(decimal_con, DACSO_Q005_DACSO_DATA_Part_1_Add_CURRENT_REGION_PSSM)
+dbExcute(decimal_con, DACSO_Q005_DACSO_DATA_Part_1_Add_CURRENT_REGION_PSSM2)
+
+# Applies weight for model year and derives New Labour Supply - re-run if changing model years or grouping geographies
+# Note: SQl may need to be updated - looks like the query needs a case_when added
+dbExcute(decimal_con, DACSO_Q005_DACSO_DATA_Part_1a_Derive)
+
+# # Refresh bgs survey records in T_Cohorts_Recoded
+dbExcute(decimal_con, DACSO_Q005_DACSO_DATA_Part_1b1_Delete_Cohort)
+dbExcute(decimal_con, DACSO_Q005_DACSO_DATA_Part_1b2_Cohort_Recoded)
+
+# Check weights
+dbExcute(decimal_con, DACSO_Q005_DACSO_DATA_Part_1b3_Check_Weights)
+
+
+
+
 # ---- Clean Up ----
 dbDisconnect(decimal_con)
