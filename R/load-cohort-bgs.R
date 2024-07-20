085ab8c11a3216906ed9d22fb24bc6b494615901
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-07-19 18:05:13 -0700

    data for last years run

--- R/load-cohort-bgs.R
+++ R/load-cohort-bgs.R
@@ -29,10 +29,10 @@ decimal_con <- dbConnect(odbc::odbc(),
 
 # ---- Read raw data from LAN ----
 # Note: some of these tables are derived and we need to figure out where they come from.  Are they rolled over year after year?
-# T_bgs_data_final_for_outcomesmatching2020 is derived in program matching and will be saved to db in future.
-# T_weights: I think this is carried forward from last models run and updated with new data.  Waiting for confirmation.
-# T_BGS_Data: carried forward from last models run and updated with new data.
-# T_BGS_INST_Recode: ?
+# T_bgs_data_final_for_outcomesmatching2020: derived in program matching and will be available as output from program matching in future.
+# T_weights: carried forward from last models run and updated with new data.  Waiting for confirmation.
+# T_BGS_Data: carried forward from last models run and updated with new data.  Look into methods so we don't have to carry forward
+# T_BGS_INST_Recode: static lookup to recode institution codes
 # bgs_current_region_data: derived in geocoding workflow and will be saved to db in future.
 
 T_bgs_data_final_for_outcomesmatching2020  <- 
@@ -52,10 +52,15 @@ T_BGS_INST_Recode <-
       col_types = cols(.default = col_character())) %>%
   janitor::clean_names(case = "all_caps")
 
-T_BGS_Data <- readr::read_csv(glue::glue("{lan}/data/student-outcomes/csv/T_BGS_Data_Final.csv"), col_types = cols(.default = col_character())) %>%
+# load T_BGS_Data_Final.csv for 2023 model run
+T_BGS_Data <- readr::read_csv(glue::glue("{lan}/data/student-outcomes/csv/T_BGS_Data_Final_2017.csv"), col_types = cols(.default = col_character())) %>%
   janitor::clean_names(case = "all_caps") %>%
-  select(-c(ID, SUBM_CD)) %>%
-  rename("NOC" = NOC_CD_2016)
+  select(-c(ID)) %>%
+  rename("NOC" = NOC_CD_2011)
+  
+  
+  #select(-c(ID, SUBM_CD)) %>%
+  #rename("NOC" = NOC_CD_2016)
           
 BGS_Data_Update <- dbGetQuery(outcomes_con, BGS_Q001_BGS_Data_2018_2019)  %>% # use BGS_Q001_BGS_Data_2020_2023 for 2023 model run
   janitor::clean_names(case = "all_caps") %>%
@@ -66,24 +71,17 @@ BGS_Data_Update <- dbGetQuery(outcomes_con, BGS_Q001_BGS_Data_2018_2019)  %>% # 
          "UNEMPLOYED" = LBR_FRC_UNEMPLOYED,
          "TRAINING_RELATED" = E10_IN_TRAINING_RELATED_JOB, 
          "TOOK_FURTH_ED" = D01_R1) %>%
-  mutate(CURRENT_ACTIVITY = case_when(
-    (EMPLOYED == 1 & D02_R1_CURRENTLY_STUDYING == 0) ~ 1, 
-    (EMPLOYED == 0 & D02_R1_CURRENTLY_STUDYING == 1) ~ 2, 
-    (EMPLOYED == 0 & D02_R1_CURRENTLY_STUDYING == 0) ~ 3,
-    (EMPLOYED == 1 & D02_R1_CURRENTLY_STUDYING == 1) ~ 4,
-    TRUE ~ 0
-  )) %>%
   mutate(SURVEY_YEAR = stringr::str_replace(SUBM_CD, "^C_Outc", "20")) %>%
   mutate(AGE_17_34 = if_else(between(AGE, 17, 34), 1, 0)) %>%
   mutate(CURRENT_REGION_PSSM_CODE = NA, 
          OLD_LABOUR_SUPPLY = NA) %>%
   select(-c(D02_R1_CURRENTLY_STUDYING, SUBM_CD))
 
-T_BGS_Data <- rbind(BGS_Data_Update, T_BGS_Data)
+T_BGS_Data_Final <- rbind(BGS_Data_Update, T_BGS_Data)
 
 # ---- write to decimal
 dbWriteTable(decimal_con, name = "T_BGS_INST_Recode", value = T_BGS_INST_Recode)
-dbWriteTable(decimal_con, name = "T_BGS_Data_Final", value = T_BGS_Data)
+dbWriteTable(decimal_con, name = "T_BGS_Data_Final2", value = T_BGS_Data_Final)
 dbWriteTable(decimal_con, name = "T_bgs_data_final_for_outcomesmatching2020", value = T_bgs_data_final_for_outcomesmatching2020)
 dbWriteTable(decimal_con, name = "T_Weights", value = T_weights)
 dbWriteTable(decimal_con, name = "bgs_current_region_data", value = bgs_current_region_data)
