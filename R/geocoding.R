deba2a738faeef036dcc3ff092cf876f3c108190
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-07-22 16:36:33 -0700

    add geocoding

---
+++ R/geocoding.R
@@ -0,0 +1,75 @@
+# ******************************************************************************
+# ---- Run Geocoding Step prior to 2b-pssm-cohorts-xxx as part of the same workflow 
+# ******************************************************************************
+library(tidyverse)
+library(RODBC)
+library(config)
+library(DBI)
+library(RJDBC)
+
+# ---- Configure LAN and file paths ----
+db_config <- config::get("decimal")
+lan <- config::get("lan")
+my_schema <- config::get("myschema")
+source(glue::glue("{lan}/development/sql/gh-source/02b-pssm-cohorts/geocoding.R"))
+
+# ---- Connection to decimal ----
+db_config <- config::get("decimal")
+decimal_con <- dbConnect(odbc::odbc(),
+                         Driver = db_config$driver,
+                         Server = db_config$server,
+                         Database = db_config$database,
+                         Trusted_Connection = "True")
+
+# ---- Check for required data tables ----
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."trd_current_region_data"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"." "')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"." "')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"." "')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"." "'))) 
+
+# ---- TRD Geocoding Step ----
+# Note: check years in queries
+dbExecute(decimal_con, "ALTER TABLE trd_current_region_data ADD current_region_pssm_code NVARCHAR(255) NULL")
+dbExecute(decimal_con, qry_000_TRD_Update_Current_Region_PSSM_step1)
+dbExecute(decimal_con, qry_000_TRD_Update_Current_Region_PSSM_step2)
+dbGetQuery(decimal_con, qry_000_TRD_results)
+
+# ---- APPSO Geocoding Step ----
+# Note: years prior to 2016 include NULL pssm codes.  These NULLs are not showing up in the 2019 model run aggregate.  
+# There was some investigation in prior years, by the looks of things so chek what is still needed to do here, if anything.
+# Note: check years in queries
+dbExecute(decimal_con, "ALTER TABLE appso_current_region_data_update ADD current_region_pssm_code NVARCHAR(255) NULL")
+dbExecute(decimal_con, "INSERT INTO appso_current_region_data SELECT * FROM appso_current_region_data_update")
+dbExecute(decimal_con, "DROP TABLE appso_current_region_data_update")
+dbExecute(decimal_con, qry_APPSO_Update_Current_Region_PSSM_step1)
+dbExecute(decimal_con, qry_APPSO_Update_Current_Region_PSSM_step2)
+dbGetQuery(decimal_con, qry_APPSO_results)
+
+# ---- DACSO Geocoding Step ----
+# Note: check years in queries
+dbExecute(decimal_con, "ALTER TABLE dacso_current_region_data_update ADD current_region_pssm_code NVARCHAR(255) NULL")
+dbExecute(decimal_con, "INSERT INTO dacso_current_region_data SELECT * FROM dacso_current_region_data_update")
+dbExecute(decimal_con, "DROP TABLE dacso_current_region_data_update")
+dbExecute(decimal_con, qry_DACSO_Update_Current_Region_PSSM_step1)
+dbExecute(decimal_con, qry_DACSO_Update_Current_Region_PSSM_step2)
+dbGetQuery(decimal_con, qry_DACSO_results)
+
+# ---- BGS Geocoding Step ----
+# Note: check years in queries
+dbExecute(decimal_con, "ALTER TABLE bgs_current_region_data_update ADD current_region_pssm_code NVARCHAR(255) NULL")
+dbExecute(decimal_con, "INSERT INTO bgs_current_region_data (stqu_id, survey_year, srv_y_n, inst, REGION_CD, CURRENT_REGION, CURRENT_REGION_NAME, POSTAL)
+          SELECT STQU_ID, SURVEY_YEAR, SRV_Y_N, INST, REGION_CD, CURRENT_REGION, CURRENT_REGION_NAME, NEW_POSTAL FROM bgs_current_region_data_update")
+dbExecute(decimal_con, "DROP TABLE bgs_current_region_data_update")
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step0)
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step1)
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step2)
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step3)
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step4)
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step4b)
+dbExecute(decimal_con, qry_BGS_update_Current_Region_PSSM_step5)
+
+dbGetQuery(decimal_con, qry_BGS_results)
+
+
+dbDisconnect(decimal_con)
---
+++ R/load-geocoding.R
@@ -0,0 +1,60 @@
+
+library(tidyverse)
+library(RODBC)
+library(config)
+library(DBI)
+library(RJDBC)
+
+
+# ---- Configure LAN and file paths ----
+db_config <- config::get("pdbtrn")
+jdbc_driver_config <- config::get("jdbc")
+lan <- config::get("lan")
+source(glue::glue("{lan}/development/sql/gh-source/02b-pssm-cohorts/geocoding.R"))
+
+# ---- Connection to outcomes ----
+jdbcDriver <- JDBC(driverClass = jdbc_driver_config$class,
+                   classPath = jdbc_driver_config$path)
+
+outcomes_con <- dbConnect(drv = jdbcDriver, 
+                          url = db_config$url,
+                          user = db_config$user,
+                          password = db_config$password)
+
+# ---- Connection to decimal ----
+db_config <- config::get("decimal")
+decimal_con <- dbConnect(odbc::odbc(),
+                         Driver = db_config$driver,
+                         Server = db_config$server,
+                         Database = db_config$database,
+                         Trusted_Connection = "True")
+
+# --- Read Geoding data from SO and write to decimal ----
+# Note: Adjust years to include 2022 and 2023 when engineering 2023 data.
+
+# trd geocoding - added all years for 2019 model run as this was the first year geocoding was done 
+trd_current_region_data <- dbGetQueryArrow(outcomes_con, qry_000_TRD_Current_Region_Data)
+dbWriteTableArrow(decimal_con, name = "trd_current_region_data", value = trd_current_region_data)
+
+# appso geocoding
+appso_current_region_data <- dbGetQueryArrow(outcomes_con, qry_APPSO_Current_Region_Data)
+dbWriteTableArrow(decimal_con, name = "appso_current_region_data_update", value = appso_current_region_data)
+
+# dacso geocoding - adding most recent years (adjust all queries so only those years are updated)
+dacso_current_region_data <- dbGetQueryArrow(outcomes_con, qry_DACSO_Current_Region_Data)
+dbWriteTableArrow(decimal_con, name = "dacso_current_region_data_update", value = dacso_current_region_data)
+
+# bgs geocoding
+bgs_current_region_data <- dbGetQuery(outcomes_con, qry_BGS_00_Append)
+new_postal <- dbGetQuery(outcomes_con, qry_BGS_00_NEW_POSTAL)
+bgs_current_region_data <- bgs_current_region_data %>% 
+  left_join(new_postal, by = join_by(STQU_ID, SURVEY_YEAR)) %>% 
+  select(-NEW_POST)
+dbWriteTableArrow(decimal_con, name = "bgs_current_region_data_update", value = bgs_current_region_data)
+
+# ---- Clean Up ---
+dbDisconnect(outcomes_con)
+dbDisconnect(decimal_con)
+
+
+                
