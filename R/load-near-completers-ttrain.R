cdecbbcec052177446d5a0401801b68c1f00f8a1
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-07-31 08:20:38 -0700

    add lookup and temp table

--- R/load-near-completers-ttrain.R
+++ R/load-near-completers-ttrain.R
@@ -25,6 +25,15 @@ outcomes_con <- dbConnect(drv = jdbcDriver,
 tmp_tbl_Age <- 
   readr::read_csv(glue::glue("{lan}/development/csv/gh-source/03-tmp_tbl_Age.csv"), col_types = cols(.default = col_guess())) %>%
   janitor::clean_names(case = "all_caps")
+stp_dacso_prgm_credential_lookup <- 
+  readr::read_csv(glue::glue("{lan}/development/csv/gh-source/STP_DACSO_PRGM_CREDENTIAL_LOOKUP.csv"), col_types = cols(.default = col_guess())) %>%
+  janitor::clean_names(case = "all_caps")
+
+# for testing of 2019 data only - remove as this table will be made in earlier workflow
+t_dacso_data_part_1 <- 
+  arrow::read_csv_arrow(glue::glue("{lan}/development/csv/gh-source/03-t_dacso_data_part_1.csv"), 
+                  col_types = cols(COCI_PEN = "c", COSC_GRAD_STATUS_LGDS_CD_Group = "c", Respondent = "c", .default = col_guess())) %>%
+  janitor::clean_names(case = "all_caps")
 
 # ---- Read SO data and disconnect ----
 source(glue::glue("{lan}/data/student-outcomes/sql/qry_make_tmp_table_Age_step1.sql"))
@@ -42,5 +51,11 @@ decimal_con <- dbConnect(odbc::odbc(),
                          Trusted_Connection = "True")
 
 dbWriteTable(decimal_con, name = "tmp_tbl_Age_AppendNewYears", value = tmp_tbl_Age_AppendNewYears)
-dbWriteTable(decimal_con, name = "tmp_tbl_Age ", value = tmp_tbl_Age )
+dbWriteTable(decimal_con, name = "tmp_tbl_Age", value = tmp_tbl_Age )
+dbWriteTable(decimal_con, name = "stp_dacso_prgm_credential_lookup", value = stp_dacso_prgm_credential_lookup)
+dbWriteTableArrow(decimal_con, name = "t_dacso_data_part_1", nanoarrow::as_nanoarrow_array(t_dacso_data_part_1arr))
 dbDisconnect(decimal_con)
+
+
+
+
