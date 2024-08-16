7fa49ad5229ee138d20cc4382ccc6b70c00164b5
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-08-16 12:36:17 -0700

    program projections code + sql calls

--- R/06-program-projections.R
+++ R/06-program-projections.R
@@ -21,19 +21,91 @@ decimal_con <- dbConnect(odbc::odbc(),
 # ---- Check for required data tables ----
 dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_DACSO_Near_Completers_RatiosAgeAtGradCIP4_TTRAIN"'))) 
 dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."Cohort_Program_Distributions_Projected"')))
-dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}".""')))
-dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}".""')))
+# tbl_Program_Projection needs to be built.  Definiton saved in Development\SQL Server\Views
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."tbl_Program_Projection_Input"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."INFOWARE_L_CIP_4DIGITS_CIP2016"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_PSSM_Projection_Cred_Grp"')))
+dbExistsTable(decimal_con, SQL(glue::glue('"{my_schema}"."T_Weights_STP"')))
+
 
 # ---- Near Completers ----
 # computes distribution of near completers
+dbExecute(decimal_con, qry_13a0_Delete_Near_Completers_Projected) 
 dbExecute(decimal_con, qry_13a0_Delete_Near_Completers_Static) 
 dbExecute(decimal_con, qry_13a1_Near_completers) 
 dbExecute(decimal_con, qry_13b_Near_Completers_Total) 
-dbExecute(decimal_con, qry_13c2_Near_Completers_Program_Dist_TTRAIN_not_used) 
 dbExecute(decimal_con, qry_13d_Append_Near_Completers_Program_Dist_Projected_TTRAIN) 
 dbExecute(decimal_con, qry_13d_Append_Near_Completers_Program_Dist_Static_TTRAIN) 
 
 # ---- Static Program Distributions ----
+# Note:	Update Survey and Projection_year for all queries
 
+dbGetQuery(decimal_con, Q012a_Check_Total_for_Invalid_CIPs) 
+dbExecute(decimal_con, Q012b_Weight_Cohort_Dist) 
+dbExecute(decimal_con, Q012c_Weighted_Cohort_Dist) 
+dbExecute(decimal_con, Q012c1_Weighted_Cohort_Dist_TTRAIN) 
+dbExecute(decimal_con, Q012c2_Weighted_Cohort_Dist) 
+dbExecute(decimal_con, Q012c3_Weighted_Cohort_Dist_Total) 
+dbExecute(decimal_con, Q012c4_Weighted_Cohort_Distribution_Projected) 
+dbExecute(decimal_con, Q012c5_Weighted_Cohort_Dist_TTRAIN) 
+dbExecute(decimal_con, Q012d_Weighted_Cohort_Dist_Total) 
+dbExecute(decimal_con, Q012e_Delete_Weighted_Cohort_Distribution) 
+dbExecute(decimal_con, Q012e_Weighted_Cohort_Distribution) 
+
+dbExecute(decimal_con, qry_12_LCP4_LCIPPC_Recode_9999)
+dbExecute(decimal_con, Q013a_Check_PDEG_CLP_07_Only_CIP_22)
+dbExecute(decimal_con, Q013b_Weight_Cohort_Dist_MAST_DOCT_Others)
+dbExecute(decimal_con, Q013c_Weighted_Cohort_Dist)
+dbExecute(decimal_con, Q013d_Weighted_Cohort_Dist_Total)
+dbExecute(decimal_con, Q013e_Weighted_Cohort_Distribution)
+
+dbExecute(decimal_con, Q014b_Weighted_Cohort_Dist_APPR)
+dbExecute(decimal_con, Q014c_Weighted_Cohort_Dist)
+dbExecute(decimal_con, Q014d_Weighted_Cohort_Dist_Total)
+dbExecute(decimal_con, Q014e_Weighted_Cohort_Distribution_Projected)
+dbExecute(decimal_con, Q014e_Weighted_Cohort_Distribution_Static )
+
+dbExecute(decimal_con, Q015e21_Append_Selected_Static_Distribution_Y2_to_Y12_Projected)
+dbExecute(decimal_con, Q015e22_Append_Distribution_Y2_to_Y12_Static)
+
+# ----  Run Werner Program ----
+# pull tbl_Program_Projections into R
+# pivot to wide format
+# pivoted data as input to program
+
+# input + output (cbind or ...) = T_Predict_CIP_CRED_AGE
+file = glue::glue("{lan}/development/csv/gh-source/testing/cip-cred-age.csv")
+T_Predict_CIP_CRED_AGE <-read_csv(file)
+dbWriteTable(decimal_con, "T_Predict_CIP_CRED_AGE", T_Predict_CIP_CRED_AGE)
+
+dbExecute(decimal_con, qry_05_Flip_T_Predict_CIP_CRED_AGE_1)
+dbExecute(decimal_con, qry_05_Flip_T_Predict_CIP_CRED_AGE_2)
+dbExecute(decimal_con, qry_09_Delete_Selected_Static_Cohort_Dist_from_Projected)
+
+dbExecute(decimal_con, qry_10a_Program_Dist_Count)
+dbExecute(decimal_con, qry_10b_Program_Dist_Total)
+dbExecute(decimal_con, qry_10c_Program_Dist_Distribution)
+# check that the changes from Q013 to qry_10c don't affect next query (it was run earlier)
+dbExecute(decimal_con, qry_12_LCP4_LCIPPC_Recode_9999)
+dbExecute(decimal_con, qry_12a_Program_Dist_Count)
+dbExecute(decimal_con, qry_12b_Program_Dist_Total)
+dbExecute(decimal_con, qry_12c_Program_Dist_Distribution)
+dbGetQuery(decimal_con, qry_12d_Check_Missing)
 
 # ---- Apprenticeship Graduates ----
+
+dbExecute(decimal_con, "drop table Q013b_Weight_Cohort_Dist_MAST_DOCT_Others")
+dbExecute(decimal_con, "drop table Q013c_Weighted_Cohort_Dist")
+dbExecute(decimal_con, "drop table Q013d_Weighted_Cohort_Dist_Total")
+dbExecute(decimal_con, "drop table Q013e_Weighted_Cohort_Distribution")
+dbExecute(decimal_con, "drop table Q012b_Weight_Cohort_Dist") 
+dbExecute(decimal_con, "drop table Q012c_Weighted_Cohort_Dist") 
+dbExecute(decimal_con, "drop table Q012c1_Weighted_Cohort_Dist_TTRAIN") 
+dbExecute(decimal_con, "drop table Q012c2_Weighted_Cohort_Dist") 
+dbExecute(decimal_con, "drop table Q012c3_Weighted_Cohort_Dist_Total") 
+dbExecute(decimal_con, "drop table Q012c4_Weighted_Cohort_Distribution_Projected") 
+dbExecute(decimal_con, "drop table Q012c5_Weighted_Cohort_Dist_TTRAIN") 
+dbExecute(decimal_con, "drop table Q012d_Weighted_Cohort_Dist_Total") 
+
+
+
--- R/load-program-projections.R
+++ R/load-program-projections.R
@@ -33,9 +33,29 @@ decimal_con <- dbConnect(odbc::odbc(),
 
 # ---- Read raw data  ----
 # Lookups
+# Note from documentation: update Y1 to model year and Y2_to_Y10 to years you want projected.  We can probably just create this table here and skip 
+# saving and uploading from year to year.
 T_Cohort_Program_Distributions_Y2_to_Y12 <-  
   readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/T_Cohort_Program_Distributions_Y2_to_Y12.csv"), 
                   col_types = cols(.default = col_guess())) %>%
+  janitor::clean_names(case = "all_caps")
+
+tbl_Age_Groups_Near_Completers <-  
+  readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/tbl_Age_Groups_Near_Completers.csv"), 
+                  col_types = cols(.default = col_guess())) %>%
+  janitor::clean_names(case = "all_caps")
+
+INFOWARE_L_CIP_4DIGITS_CIP2016 <- dbGetQuery(outcomes_con, "SELECT * FROM L_CIP_4DIGITS_CIP2016")
+INFOWARE_L_CIP_6DIGITS_CIP2016 <- dbGetQuery(outcomes_con, "SELECT * FROM L_CIP_6DIGITS_CIP2016")
+
+T_PSSM_Projection_Cred_Grp  <- 
+  readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/T_PSSM_Projection_Cred_Grp.csv"), 
+                  col_types = cols(.default = col_guess())) %>%
+  janitor::clean_names(case = "all_caps")
+
+T_Weights_STP <- 
+readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/T_Weights_STP.csv"), 
+                col_types = cols(.default = col_guess())) %>%
   janitor::clean_names(case = "all_caps")
 
 
@@ -50,18 +70,44 @@ Cohort_Program_Distributions_Static <-
                   col_types = cols(.default = col_guess())) %>%
   janitor::clean_names(case = "all_caps")
 
+
+T_Cohorts_Recoded <-
+  readr::read_csv(glue::glue("{lan}/development/csv/gh-source/testing/T_Cohorts_Recoded.csv"),
+                  col_types = cols(.default = col_guess())) %>%
+  janitor::clean_names(case = "all_caps")
+
 # ---- Write to decimal ----
 dbWriteTable(decimal_con,
-             name = "T_Cohort_Program_Distributions_Y2_to_Y12",
-             T_Cohort_Program_Distributions_Y2_to_Y12)
+             name = "tbl_Age_Groups_Near_Completers",
+             tbl_Age_Groups_Near_Completers)
+
+dbWriteTable(decimal_con,
+             name = "INFOWARE_L_CIP_4DIGITS_CIP2016",
+             INFOWARE_L_CIP_4DIGITS_CIP2016)
+dbWriteTable(decimal_con,
+             name = "INFOWARE_L_CIP_6DIGITS_CIP2016",
+             INFOWARE_L_CIP_6DIGITS_CIP2016)
+
+dbWriteTable(decimal_con,
+             name = "T_PSSM_Projection_Cred_Grp",
+             T_PSSM_Projection_Cred_Grp)
+
+dbWriteTable(decimal_con,
+             name = "T_Weights_STP",
+             T_Weights_STP)
+
+dbWriteTable(decimal_con,
+             name = "Cohort_Program_Distributions_Static",
+             Cohort_Program_Distributions_Static)
 
 dbWriteTable(decimal_con,
              name = "Cohort_Program_Distributions_Projected",
              Cohort_Program_Distributions_Projected)
 
 dbWriteTable(decimal_con,
-             name = "Cohort_Program_Distributions_Static",
-             Cohort_Program_Distributions_Static)
+             name = "T_Cohorts_Recoded",
+             T_Cohorts_Recodedc)
+
 
 # ---- Disconnect ----
 dbDisconnect(decimal_con)
