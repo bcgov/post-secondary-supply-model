6127cec66410ab81a6bddc30ca46ab788f478923
Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-08-31 15:07:31 -0700

    move to sql, some review and checks

--- R/load-program-projections.R
+++ R/load-program-projections.R
@@ -1,5 +1,18 @@
 # ******************************************************************************
 # Load datasets required to run program projections step
+# Note: Rollover dataset originally contain these entries in SURVEY:
+# Projected
+#  - PTIB                                    
+#  - Program_Projections_2019-2020_Q014e   
+#  - Program_Projections_2019-2020_qry10c 
+#  - Program_Projections_2019-2020_qry12c   
+#  - Program_Projections_2019-2020_qry_13d 
+# Static
+#  - PTIB                                 
+#  - Program_Projections_2019-2020_Q012e
+#  - Program_Projections_2019-2020_Q013e
+#  - Program_Projections_2019-2020_Q014e
+#  - Program_Projections_2019-2020_qry_13d
 # ******************************************************************************
 
 library(tidyverse)
@@ -57,17 +70,12 @@ readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/06/T_Weights
   janitor::clean_names(case = "all_caps")
 
 AgeGroupLookup <- 
-  readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/06/AgeGroupLookup.csv"),  col_types = cols(.default = col_guess())) %>%
-  janitor::clean_names(case = "all_caps")
+  readr::read_csv(glue::glue("{lan}/development/csv/gh-source/lookups/06/AgeGroupLookup.csv"),  col_types = cols(.default = col_guess()))
 
-# From outcomes
+# ---- Read from Student Outcomes ----
 INFOWARE_L_CIP_4DIGITS_CIP2016 <- dbGetQuery(outcomes_con, "SELECT * FROM L_CIP_4DIGITS_CIP2016")
 INFOWARE_L_CIP_6DIGITS_CIP2016 <- dbGetQuery(outcomes_con, "SELECT * FROM L_CIP_6DIGITS_CIP2016")
 
-# check that only required survey years are in T_Cohorts_Recoded
-stopifnot(exprs = {
-  dbGetQuery(decimal_con, "select distinct survey_year from T_Cohorts_Recoded")$survey_year==c(2019:2023)
-})
 
 # ---- Rollover data ----
 Cohort_Program_Distributions_Projected <-
@@ -78,63 +86,31 @@ Cohort_Program_Distributions_Static <-
   readr::read_csv(glue::glue("{lan}/development/csv/gh-source/rollover/06/Cohort_Program_Distributions_Static.csv"),  col_types = cols(.default = col_guess())) %>%
   janitor::clean_names(case = "all_caps")
 
-
 # ---- Build tbl_Program_Projection_Input ---- 
-qry_Build_Program_Projection_Input <- "
---CREATE VIEW tbl_Program_Projection_Input AS 
-SELECT  A.AgeGroup, 
-        tblCredential_HighestRank.PSI_CREDENTIAL_CATEGORY, 
-        tblCredential_HighestRank.PSI_CREDENTIAL_CATEGORY + A.AgeGroup AS Expr1,
-        Credential_Non_Dup.FINAL_CIP_CODE_4, 
-        tblCredential_HighestRank.PSI_AWARD_SCHOOL_YEAR_DELAYED, 
-COUNT(*) AS Count 
-FROM    tblCredential_HighestRank 
-INNER JOIN pssm2019.dbo.AgeGroupLookup A
-  ON    tblCredential_HighestRank.AGE_GROUP_AT_GRAD = A.AgeIndex 
-INNER JOIN Credential_Non_Dup 
-  ON    tblCredential_HighestRank.id = Credential_Non_Dup.id 
-WHERE   (tblCredential_HighestRank.PSI_VISA_STATUS = 'DOMESTIC') 
-  AND   (tblCredential_HighestRank.RESEARCH_UNIVERSITY = 1) 
-  AND   (tblCredential_HighestRank.OUTCOMES_CRED <> 'DACSO') 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '09') 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '10') 
-  OR    (tblCredential_HighestRank.PSI_VISA_STATUS IS NULL) 
-  AND   (tblCredential_HighestRank.RESEARCH_UNIVERSITY = 1) 
-  AND   (tblCredential_HighestRank.OUTCOMES_CRED <> 'DACSO') 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '09') 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '10') 
-  OR    (tblCredential_HighestRank.PSI_VISA_STATUS = 'DOMESTIC') 
-  AND   (tblCredential_HighestRank.RESEARCH_UNIVERSITY IS NULL) 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '09') 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '10') 
-  OR    (tblCredential_HighestRank.PSI_VISA_STATUS IS NULL) 
-  AND   (tblCredential_HighestRank.RESEARCH_UNIVERSITY IS NULL) 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '09') 
-  AND   (Credential_Non_Dup.FINAL_CIP_CLUSTER_CODE <> '10') 
-GROUP BY A.AgeGroup, 
-        tblCredential_HighestRank.PSI_CREDENTIAL_CATEGORY, 
-        tblCredential_HighestRank.PSI_AWARD_SCHOOL_YEAR_DELAYED,  
-        Credential_Non_Dup.FINAL_CIP_CODE_4 
-HAVING  (tblCredential_HighestRank.PSI_CREDENTIAL_CATEGORY <> 'APPRENTICESHIP')"
-
 tbl_Program_Projection_Input <- dbGetQuery(decimal_con, qry_Build_Program_Projection_Input)
 
 # ---- Write to decimal ----
+dbWriteTable(decimal_con, name = "AgeGroupLookup", AgeGroupLookup)
 dbWriteTable(decimal_con, name = "tbl_Age_Groups_Near_Completers", tbl_Age_Groups_Near_Completers)
-dbWriteTable(decimal_con, name = "tbl_Age_Groups", tbl_Age_Groups, overwrite=TRUE)
+dbWriteTable(decimal_con, name = "tbl_Age_Groups", tbl_Age_Groups)
 dbWriteTable(decimal_con, name = "T_Cohort_Program_Distributions_Y2_to_Y12",  T_Cohort_Program_Distributions_Y2_to_Y12)
 dbWriteTable(decimal_con, name = "T_APPR_Y2_to_Y10",  T_APPR_Y2_to_Y10)
 dbWriteTable(decimal_con, name = "INFOWARE_L_CIP_4DIGITS_CIP2016", INFOWARE_L_CIP_4DIGITS_CIP2016)
 dbWriteTable(decimal_con, name = "INFOWARE_L_CIP_6DIGITS_CIP2016", INFOWARE_L_CIP_6DIGITS_CIP2016)
 dbWriteTable(decimal_con, name = "T_PSSM_Projection_Cred_Grp", T_PSSM_Projection_Cred_Grp)
 dbWriteTable(decimal_con, name = "T_Weights_STP",  T_Weights_STP)
-
 dbWriteTable(decimal_con, name = "tbl_Program_Projection_Input", tbl_Program_Projection_Input)
 
 # ---- Rollover ----
 dbWriteTable(decimal_con, name = "Cohort_Program_Distributions_Static",  Cohort_Program_Distributions_Static)
+dbGetQuery(decimal_con, "delete from Cohort_Program_Distributions_Static")
 dbWriteTable(decimal_con, name = "Cohort_Program_Distributions_Projected",  Cohort_Program_Distributions_Projected)
+dbGetQuery(decimal_con, "delete from Cohort_Program_Distributions_Projected")
 
+# check that only required survey years are in T_Cohorts_Recoded
+stopifnot(exprs = {
+  dbGetQuery(decimal_con, "select distinct survey_year from T_Cohorts_Recoded")$survey_year==c(2019:2023)
+})
 
 # ---- Disconnect ----
 dbDisconnect(decimal_con)
