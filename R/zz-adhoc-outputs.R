3b1f60f0d57a372e52460225b87f3ee46f10cb72
Lowery <Amelia.Lowery@gov.bc.ca>
2024-09-25 16:19:09 -0700

    custom 2 digit noc output request

--- R/zz-adhoc-outputs.R
+++ R/zz-adhoc-outputs.R
@@ -48,3 +48,68 @@ data_2_NOC <- data %>%
 write_csv(data_2_NOC,glue::glue(folder_path,"public_release_static_no_ptib_2_digit_rollup.csv"))
 
 # ******************************************************************************
+# ---- Rollup NOCs custom groupings ----
+## ---- libraries and global variables
+library(RODBC)
+library(arrow)
+library(tidyverse)
+library(odbc)
+library(RJDBC) ## loads DBI
+
+# Setup
+## ---- Configure LAN and file paths ----
+lan <- config::get("lan")
+my_schema <- config::get("myschema")
+folder_path <- glue::glue("{lan}/development/work/adhoc-outputs/")
+
+## ---- Connection to database ----
+db_config <- config::get("decimal")
+decimal_con <- dbConnect(odbc::odbc(),
+                         Driver = db_config$driver,
+                         Server = db_config$server,
+                         Database = db_config$database,
+                         Trusted_Connection = "True")
+
+## ---- Read draft data files ----
+public_data <-  tbl(decimal_con, dbplyr::in_schema(sql('"IDIR\\ALOWERY"'), "qry_10a_Model_Public_Release_Union")) %>% collect()
+tbl_model <- tbl(decimal_con, dbplyr::in_schema(sql('"IDIR\\ALOWERY"'), "tmp_tbl_Model")) %>% collect()
+
+## ---- Read lookup file ----
+noc_rollup <- read_csv(glue::glue(folder_path,"brett_custom_noc_rollup.csv"))
+
+## Public data ----
+### ---- Create sub NOCs public data ----
+public_data_prep <- public_data %>% 
+  mutate(NOC_2 = ifelse(NOC=="99998","N/A",str_sub(NOC,1,2))) %>% 
+  left_join(noc_rollup, by="NOC_2") %>% 
+  select(-starts_with("Current_Region"),-Expr1,-ENGLISH_NAME,-NOC)
+
+### ---- Get results by custom 2-digit NOC rollup ----
+public_data_custom_NOC <- public_data_prep %>% 
+  group_by(Age_Group_Rollup_Label,NOC_Custom_Group) %>% 
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE))) %>% ungroup()
+
+write_csv(public_data_custom_NOC,glue::glue(folder_path,"public_data_custom_NOC_rollup_20240925.csv"))
+
+## Full Model ----
+### ---- Create sub NOCs full model data ----
+model_data_prep <- tbl_model %>% 
+  filter(Current_Region_PSSM_Code_Rollup==5900) %>% 
+  # mutate(NOC_level = str_length(NOC)) %>% 
+  # filter(NOC_level==2) %>% 
+  filter(NOC_Level==2) %>% 
+  left_join(noc_rollup, by=c("NOC"="NOC_2")) %>% 
+  select(-starts_with("Current_Region"),-Expr1,-ENGLISH_NAME,-NOC,-NOC_Level)
+
+### ---- Get results by custom 2-digit NOC rollup ----
+model_data_custom_NOC <- model_data_prep %>% 
+  group_by(Age_Group_Rollup_Label,NOC_Custom_Group) %>% 
+  summarise(across(where(is.numeric), ~ sum(.x, na.rm = TRUE))) %>% ungroup()
+
+write_csv(public_data_custom_NOC,glue::glue(folder_path,"full_model_data_custom_NOC_rollup_20240925.csv"))
+
+## ---- Clean up ----
+dbDisconnect(decimal_con)
+rm(list=ls())
+
+# ******************************************************************************
