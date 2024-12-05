ef29a64c79109859638fe63fc0d69751d3ac2fa1
Bonnie Ashcroft <Bonnie.Ashcroft@gov.bc.ca>
2024-12-05 14:11:47 -0800

    fiddle with layout

--- R/load-outcomes-data.R
+++ R/load-outcomes-data.R
@@ -31,24 +31,41 @@
 #   INFOWARE_L_CIP_6DIGITS_CIP2016
 #############################################################################################################################################
 
+#---------------------------------------------------------------------------------------------------------------------------
+# notes: 
+#   minor differences in code description/name (for sml n) in INFOWARE_L_CIP_4DIGITS_CIP2016, INFOWARE_L_CIP_4DIGITS_CIP2016
+#   differences in prgm_credential (for sml n) in T_DACSO_DATA_Part_1_stepA
+#   TODO: handle BGS seperatly
+#---------------------------------------------------------------------------------------------------------------------------
+
 library(tidyverse)
 library(config)
+library(RODBC)
+library(DBI)
+
+# set up db connection and lan paths
+db_config <- config::get("decimal")
+decimal_con <- dbConnect(odbc::odbc(),
+                 Driver = db_config$driver,
+                 Server = db_config$server,
+                 Database = db_config$database,
+                 Trusted_Connection = "True")
 
 lan = config::get("lan")
-
 so_lan_path <- glue::glue("{lan}/data/student-outcomes/csv/so-provision/")
 
 # read csv's into objects in memory.
 fls <- list.files(so_lan_path, pattern = ".csv", full.names = TRUE)
 tmp_table_age_fls <- list.files(so_lan_path, pattern = "qry_make_tmp_table_Age_step1_20[0-9][0-9].csv", full.names = TRUE)
 
+# I've read that using assign this way is for novice programmers ... it works though, so..?
 fls %>%  
   set_names(tools::file_path_sans_ext(basename(fls))) %>% 
   map(read_csv, show_col_types = FALSE) %>%
   imap(~ assign(..2, ..1, envir = .GlobalEnv)) %>%
   invisible()
 
-#reloads tmp_table_age_fls so a bit of a hack, but it's the cleanest way I can think of
+# reloads tmp_table_age_fls so a bit of a hack, but it's the cleanest way I can think of
 tmp_table_age_fls %>%  
   set_names(tools::file_path_sans_ext(basename(tmp_table_age_fls))) %>% 
   map(read_csv, show_col_types = FALSE, col_types="dcdcd") %>%
@@ -56,7 +73,7 @@ tmp_table_age_fls %>%
   invisible()
 
 # combine tmp_table_Age_step1_20xx
-ls(patt="tmp_table_Age_step1_20") %>% 
+tmp_table_Age <- ls(patt="tmp_table_Age_20") %>% 
   mget(envir = .GlobalEnv) %>%
   bind_rows()
 
@@ -75,29 +92,13 @@ DACSO_Q003_DACSO_DATA_Part_1_stepA$TPID_LGND_CD = as.character(DACSO_Q003_DACSO_
 
 BGS_Q001_BGS_Data_2019_2023$PEN = as.character(BGS_Q001_BGS_Data_2019_2023$PEN)
 
-
-# write to decimal
-dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."T_APPSO_Data_Final"')),  value = APPSO_Data_Final)
-dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."APPSO_Graduates"')),  value = APPSO_Graduates)
-dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."TRD_Graduates"')),  value = Q000_TRD_Graduates)
-dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."INFOWARE_L_CIP_6DIGITS_CIP2016"')),  value = INFOWARE_L_CIP_6DIGITS_CIP2016)
-dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."INFOWARE_L_CIP_4DIGITS_CIP2016"')),  value = INFOWARE_L_CIP_4DIGITS_CIP2016)
-dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."infoware_c_outc_clean_short_resp"')),  value = infoware_c_outc_clean_short_resp)
+# write to decimal.  TODO: code this as a list or function.
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."T_APPSO_Data_Final_raw"')),  value = APPSO_Data_Final)
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."APPSO_Graduates_raw"')),  value = APPSO_Graduates)
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."TRD_Graduates_raw"')),  value = Q000_TRD_Graduates)
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."INFOWARE_L_CIP_6DIGITS_CIP2016_raw"')),  value = INFOWARE_L_CIP_6DIGITS_CIP2016)
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."INFOWARE_L_CIP_4DIGITS_CIP2016_raw"')),  value = INFOWARE_L_CIP_4DIGITS_CIP2016)
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."infoware_c_outc_clean_short_resp_raw"')),  value = infoware_c_outc_clean_short_resp)
+dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."tmp_table_Age_raw"')),  value = tmp_table_Age)
 #dbWriteTable(decimal_con, overwrite = TRUE, name = SQL(glue::glue('"dbo"."BGS_Q001_BGS_Data_2019_2023"')),  value = BGS_Q001_BGS_Data_2019_2023)
 
-#*************************************************************************************************
-# QA scratch stuff only - I'll remove from here down when finished
-# Q000_TRD_DATA_01 is corrupt
-# minor differences in code description/name for sml n rows in INFOWARE_L_CIP_4DIGITS_CIP2016, INFOWARE_L_CIP_4DIGITS_CIP2016
-# differences in prgm_credential for a sml n rows in T_DACSO_DATA_Part_1_stepA
-# handle BGS seperatly
-#*************************************************************************************************
-library(RODBC)
-library(DBI)
-
-db_config <- config::get("decimal")
-decimal_con <- dbConnect(odbc::odbc(),
-                 Driver = db_config$driver,
-                 Server = db_config$server,
-                 Database = db_config$database,
-                 Trusted_Connection = "True")
