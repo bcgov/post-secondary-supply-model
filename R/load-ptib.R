5fddbd277cb3e24b979c5739fe2aec1a220f2a11
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-03-18 10:59:44 -0700

    clean script header

--- R/load-ptib.R
+++ R/load-ptib.R
@@ -10,10 +10,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-# ******************************************************************************
-# Load PTIB enrolment data from staging area in LAN project folder, to decimal.
-# Raw data is in excel so some cleaning req'd to handle cip code conversion issues
-# ******************************************************************************
 library(tidyverse)
 library(odbc)
 library(DBI)
