a21d505cd4d02a2ecf66b0b85e05f56714444f4d
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-04-01 13:13:10 -0700

    read from lan+apply air formatting

--- R/load-cohort-trd.R
+++ R/load-cohort-trd.R
@@ -10,7 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-# This script loads student outcomes data for students who students who were formerly enrolled in 
+# This script loads student outcomes data for students who students who were formerly enrolled in
 # a trades program (i.e. an apprenticeship, trades foundation program or trades-related vocational program)
 #
 # The following data sets are read into SQL server from the student outcomes survey database:
@@ -30,56 +30,92 @@ lan <- config::get("lan")
 
 # ---- Connection to decimal ----
 db_config <- config::get("decimal")
-decimal_con <- dbConnect(odbc::odbc(),
-                         Driver = db_config$driver,
-                         Server = db_config$server,
-                         Database = db_config$database,
-                         Trusted_Connection = "True")
+decimal_con <- dbConnect(
+  odbc::odbc(),
+  Driver = db_config$driver,
+  Server = db_config$server,
+  Database = db_config$database,
+  Trusted_Connection = "True"
+)
 # should specify the DBO schema for final run, individual IDIRS for testing
 schema <- config::get("myschema")
 
 # ---- Read raw data and disconnect ----
 #source(glue::glue("./sql/02b-pssm-cohorts/trd-data.sql"))
 
-Q000_TRD_DATA_01 <- dbReadTable(decimal_con, SQL(glue::glue('"{my_schema}"."Q000_TRD_DATA_01_raw"')))
-Q000_TRD_Graduates <- dbReadTable(decimal_con, SQL(glue::glue('"{my_schema}"."Q000_TRD_Graduates_raw"')))
+Q000_TRD_DATA_01 <- read_csv(glue::glue(
+  "{lan}/data/student-outcomes/csv/so-provision/Q000_TRD_DATA_01.csv"
+))
+
+Q000_TRD_Graduates <- read_csv(glue::glue(
+  "{lan}/data/student-outcomes/csv/so-provision/Q000_TRD_Graduates.csv"
+))
 
 # Convert some variables that should be numeric
-Q000_TRD_DATA_01 <- Q000_TRD_DATA_01 %>% 
-  mutate(GRADSTAT = as.numeric(GRADSTAT), 
-         KEY = as.numeric(KEY),  
-         TTRAIN = as.numeric(TTRAIN))
+Q000_TRD_DATA_01 <- Q000_TRD_DATA_01 %>%
+  mutate(
+    GRADSTAT = as.numeric(GRADSTAT),
+    KEY = as.numeric(KEY),
+    TTRAIN = as.numeric(TTRAIN)
+  )
 
 # Gradstat group : couldn't find in outcomes data so defining here.
-Q000_TRD_DATA_01 <- Q000_TRD_DATA_01 %>% 
-  mutate(LCIP4_CRED = paste0(GRADSTAT_GROUP, ' - ' , LCIP_LCP4_CD , ' - ' , TTRAIN , ' - ' , PSSM_CREDENTIAL))
+Q000_TRD_DATA_01 <- Q000_TRD_DATA_01 %>%
+  mutate(
+    LCIP4_CRED = paste0(
+      GRADSTAT_GROUP,
+      ' - ',
+      LCIP_LCP4_CD,
+      ' - ',
+      TTRAIN,
+      ' - ',
+      PSSM_CREDENTIAL
+    )
+  )
 
 Q000_TRD_DATA_01 <-
-  Q000_TRD_DATA_01 %>% 
-  mutate(CURRENT_REGION_PSSM_CODE =  case_when (
-    CURRENT_REGION1 %in% 1:8 ~ CURRENT_REGION1, 
-    CURRENT_REGION4 == 5 ~ 9,
-    CURRENT_REGION4 == 6 ~ 10,
-    CURRENT_REGION4 == 7 ~ 11,
-    CURRENT_REGION4 == 8 ~ -1,
-    TRUE ~ NA)) 
+  Q000_TRD_DATA_01 %>%
+  mutate(
+    CURRENT_REGION_PSSM_CODE = case_when(
+      CURRENT_REGION1 %in% 1:8 ~ CURRENT_REGION1,
+      CURRENT_REGION4 == 5 ~ 9,
+      CURRENT_REGION4 == 6 ~ 10,
+      CURRENT_REGION4 == 7 ~ 11,
+      CURRENT_REGION4 == 8 ~ -1,
+      TRUE ~ NA
+    )
+  )
 
 
 # prepare graduate dataset
-Q000_TRD_Graduates   %>%
-  mutate(AGE_GROUP_LABEL = case_when (
-    TRD_AGE_AT_SURVEY %in% 15:16 ~ "15 to 16",
-    TRD_AGE_AT_SURVEY %in% 17:19 ~ "17 to 19",
-    TRD_AGE_AT_SURVEY %in% 20:24 ~ "20 to 24",
-    TRD_AGE_AT_SURVEY %in% 25:29 ~ "25 to 29",
-    TRD_AGE_AT_SURVEY %in% 30:34 ~ "30 to 34",
-    TRD_AGE_AT_SURVEY %in% 35:44 ~ "35 to 44",
-    TRD_AGE_AT_SURVEY %in% 45:54 ~ "45 to 54",
-    TRD_AGE_AT_SURVEY %in% 55:64 ~ "55 to 64",
-    TRD_AGE_AT_SURVEY %in% 65:89 ~ "65 to 89",
-    TRUE ~ NA)) -> Q000_TRD_Graduates 
+Q000_TRD_Graduates %>%
+  mutate(
+    AGE_GROUP_LABEL = case_when(
+      TRD_AGE_AT_SURVEY %in% 15:16 ~ "15 to 16",
+      TRD_AGE_AT_SURVEY %in% 17:19 ~ "17 to 19",
+      TRD_AGE_AT_SURVEY %in% 20:24 ~ "20 to 24",
+      TRD_AGE_AT_SURVEY %in% 25:29 ~ "25 to 29",
+      TRD_AGE_AT_SURVEY %in% 30:34 ~ "30 to 34",
+      TRD_AGE_AT_SURVEY %in% 35:44 ~ "35 to 44",
+      TRD_AGE_AT_SURVEY %in% 45:54 ~ "45 to 54",
+      TRD_AGE_AT_SURVEY %in% 55:64 ~ "55 to 64",
+      TRD_AGE_AT_SURVEY %in% 65:89 ~ "65 to 89",
+      TRUE ~ NA
+    )
+  ) -> Q000_TRD_Graduates
 
-dbWriteTable(decimal_con, name = SQL(glue::glue('"{schema}"."T_TRD_DATA"')), value = Q000_TRD_DATA_01, overwrite = TRUE)
-dbWriteTable(decimal_con, name = SQL(glue::glue('"{schema}"."TRD_Graduates"')), value = Q000_TRD_Graduates, overwrite = TRUE)
+dbWriteTable(
+  decimal_con,
+  name = SQL(glue::glue('"{my_schema}"."T_TRD_DATA"')),
+  value = Q000_TRD_DATA_01,
+  overwrite = TRUE
+)
+
+dbWriteTable(
+  decimal_con,
+  name = SQL(glue::glue('"{my_schema}"."TRD_Graduates"')),
+  value = Q000_TRD_Graduates,
+  overwrite = TRUE
+)
 
 dbDisconnect(decimal_con)
