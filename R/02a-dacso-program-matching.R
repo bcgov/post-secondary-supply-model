10bc192f6a88de65d47c6f967759c58d159057dc
Jon Duan <jon.duan@gov.bc.ca>
2026-04-24 23:05:29 -0700

    commit before pull

--- R/02a-dacso-program-matching.R
+++ R/02a-dacso-program-matching.R
@@ -1143,7 +1143,11 @@ xwalk_exact <- xwalk %>%
     PSI_CREDENTIAL_PROGRAM_DESC_KEY,
     CIP_CODE_4,
     LCP4_CIP_4DIGITS_NAME
-  ) |> slice_head(n=1, by = c(PSI_CODE_KEY, PSI_PROGRAM_CODE_KEY, PSI_CREDENTIAL_PROGRAM_DESC_KEY))
+  ) |>
+  slice_head(
+    n = 1,
+    by = c(PSI_CODE_KEY, PSI_PROGRAM_CODE_KEY, PSI_CREDENTIAL_PROGRAM_DESC_KEY)
+  )
 
 stp_dacso <- stp_dacso %>%
   left_join(
@@ -1153,8 +1157,7 @@ stp_dacso <- stp_dacso %>%
       "PSI_CODE_KEY" = "PSI_CODE_KEY",
       "PSI_PROGRAM_CODE_KEY" = "PSI_PROGRAM_CODE_KEY",
       "PSI_CREDENTIAL_PROGRAM_DESCRIPTION_KEY" = "PSI_CREDENTIAL_PROGRAM_DESC_KEY"
-    ),
-    relationship = "many-to-many" # no reason is provided
+    )
   ) %>%
   mutate(
     Already_Matched = if_else(
@@ -1167,11 +1170,6 @@ stp_dacso <- stp_dacso %>%
   ) %>%
   select(-XW_CIP4, -XW_CIP4_NAME)
 
-
-# ℹ Row 14 of `x` matches multiple rows in `y`.
-# ℹ Row 2465 of `y` matches multiple rows in `x`.
-stp_dacso <- stp_dacso %>% distinct() # thia is not in the original code
-# 5462
 
 # join STP data to XWALK on COCI_INST_CD, PSI_CREDENTIAL_PROGRAM_DESCRIPTION, PSI_PROGRAM_CODE
 # Match on COCI_INST_CD + PSI_PROGRAM_CODE + PSI_CREDENTIAL_PROGRAM_DESCRIPTION
