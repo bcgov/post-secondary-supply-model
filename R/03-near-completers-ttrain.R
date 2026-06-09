529f7e681229db33ce31aab3f7b38797523c8c0b
Bonnie Ashcroft <bonnie.ashcroft@gov.bc.ca>
2026-06-09 14:15:34 -0700

    Apply suggestion from @BonnieJRobert

--- R/03-near-completers-ttrain.R
+++ R/03-near-completers-ttrain.R
@@ -1116,10 +1116,12 @@ near_completes_total_by_cip4_ttrain_history <- t_dacso_data_part_1 |>
   filter(
     cosc_grad_status_lgds_cd_group == "3"
   ) |>
-  # inner_join(
-  #   age_group_lookup,
-  #   by = join_by(age_at_grad >= lower_bound, age_at_grad <= upper_bound)
-  # ) |>
+select(-age_group)
+  # age group already introduced from lookup table
+   inner_join(
+     age_group_lookup,
+     by = join_by(age_at_grad >= lower_bound, age_at_grad <= upper_bound)
+   ) |>
   left_join(
     credential_rank,
     by = c("prgm_credential_awarded_name" = "psi_credential_category")
